cb52221a1ae021dce4c083a657801e1da2ceb46f
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2025-01-03 03:55:04 +0100

    Update old-events.js

--- scripts/old-events.js
+++ scripts/old-events.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const TEST_MODE = true; // Teszt mód: kézi idő beállítása
-    const TEST_TIME = "2025-01-05T11:30:00";
+    const TEST_TIME = "2025-01-04T11:30:00";
     const now = TEST_MODE ? new Date(TEST_TIME) : new Date(); // Aktuális idő
 
     // Minden session elem feldolgozása
