31115497edb5f28d43e3d181023b3d0fb475eae7
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2025-01-03 01:01:33 +0100

    Update css-grid-template.php

--- styles/css-grid-template.php
+++ styles/css-grid-template.php
@@ -16,7 +16,7 @@ $last = strtotime(end($jsonData[0]['programs'])['endTime']);  // Utolsó program
 
 $v_space = '5fr';
 $end_space = '10px';
-$gap = '5'; // percek
+$gap = '2'; // percek
 $timing_space = '1px';
 
 echo '
