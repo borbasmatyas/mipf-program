ef56950396a2bb82edba14171fac7abeac4496e3
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2026-01-09 09:49:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -83,11 +83,13 @@
 						if ($branchName):
 
 					?>
-					<svg aria-hidden="true" focusable="false" class="octicon octicon-git-branch" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom">
-						<path d="M9.5 3.25a2.25 2.25 0 1 1 3 2.122V6A2.5 2.5 0 0 1 10 8.5H6a1 1 0 0 0-1 1v1.128a2.251 2.251 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.5 0v1.836A2.493 2.493 0 0 1 6 7h4a1 1 0 0 0 1-1v-.628A2.25 2.25 0 0 1 9.5 3.25Zm-6 0a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Zm8.25-.75a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM4.25 12a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Z">
-						</path>
-					</svg>
-					<span class="repo-footer-link-item"><?php echo htmlspecialchars($branchName); ?></span>
+					<a href="https://github.com/borbasmatyas/mipf-program/tree/<?php echo urlencode($branchName); ?>" target="_blank" rel="noopener noreferrer" title="Aktuális ág: <?php echo htmlspecialchars($branchName); ?>" class="repo-footer-link-item">
+						<svg aria-hidden="true" focusable="false" class="octicon octicon-git-branch" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" display="inline-block" overflow="visible" style="vertical-align:text-bottom">
+							<path d="M9.5 3.25a2.25 2.25 0 1 1 3 2.122V6A2.5 2.5 0 0 1 10 8.5H6a1 1 0 0 0-1 1v1.128a2.251 2.251 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.5 0v1.836A2.493 2.493 0 0 1 6 7h4a1 1 0 0 0 1-1v-.628A2.25 2.25 0 0 1 9.5 3.25Zm-6 0a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Zm8.25-.75a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM4.25 12a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Z">
+							</path>
+						</svg>
+						<span class="repo-footer-link-item"><?php echo htmlspecialchars($branchName); ?></span>
+					</a>
 					<?php
 						endif;
 					?>
