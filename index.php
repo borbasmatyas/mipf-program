1817624ccd9a901e4bc124123f4a2557dafa5ac4
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2026-01-13 11:21:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 		<meta name="apple-mobile-web-app-title" content="Fesztivál program">
 		<link rel="manifest" href="/manifest.json">
-		<link rel="apple-touch-icon" href="/images/icons/apple-touch-icon.png">
 		<link rel="stylesheet" href="/styles/main.css">
 		<link rel="stylesheet" href="/styles/program.css">
 		<link rel="stylesheet" href="/styles/css-grid-template.php?<?php echo date('U'); ?>">
